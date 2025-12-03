20056817a62f12ca824d1f19485232fd360e77fb
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-12-03 08:24:05 -0700

    feat: generate script

--- generate.sh
+++ generate.sh
@@ -1,5 +1,52 @@
 #!/bin/bash
-uv run python datasets/process_zone.py --data-path /data/plant-rl/online/E14/P1/Constant1/alliance-zone01/
-uv run python datasets/process_zone.py --data-path /data/plant-rl/online/E14/P1/InAC2/alliance-zone02/
-uv run python datasets/join_zones.py --root-dir /data/plant-rl/online --output-dir /data/plant-rl/offline/v16/
-uv run python datasets/create_minari_dataset.py --input-dir /data/plant-rl/offline/v16/
+
+uv run python process_zone.py --data-path /data/plant-rl/online/E11/P1/DiscreteRandom1/alliance-zone01
+uv run python process_zone.py --data-path /data/plant-rl/online/E11/P1/DiscreteRandom2/alliance-zone02
+uv run python process_zone.py --data-path /data/plant-rl/online/E11/P1/DiscreteRandom3/alliance-zone03
+uv run python process_zone.py --data-path /data/plant-rl/online/E11/P1/DiscreteRandom4/alliance-zone04
+uv run python process_zone.py --data-path /data/plant-rl/online/E11/P1/DiscreteRandom5/alliance-zone05
+uv run python process_zone.py --data-path /data/plant-rl/online/E11/P1/DiscreteRandom6/alliance-zone06
+uv run python process_zone.py --data-path /data/plant-rl/online/E11/P1/DiscreteRandom8/alliance-zone08
+uv run python process_zone.py --data-path /data/plant-rl/online/E11/P1/DiscreteRandom9/alliance-zone09
+uv run python process_zone.py --data-path /data/plant-rl/online/E11/P1/DiscreteRandom10/alliance-zone10
+uv run python process_zone.py --data-path /data/plant-rl/online/E11/P1/DiscreteRandom11/alliance-zone11
+uv run python process_zone.py --data-path /data/plant-rl/online/E11/P1/DiscreteRandom12/alliance-zone12
+
+uv run python process_zone.py --data-path /data/plant-rl/online/E12/P1/DiscreteRandom1/alliance-zone01
+uv run python process_zone.py --data-path /data/plant-rl/online/E12/P1/DiscreteRandom2/alliance-zone02
+uv run python process_zone.py --data-path /data/plant-rl/online/E12/P1/DiscreteRandom3/alliance-zone03
+uv run python process_zone.py --data-path /data/plant-rl/online/E12/P1/DiscreteRandom4/alliance-zone04
+uv run python process_zone.py --data-path /data/plant-rl/online/E12/P1/DiscreteRandom5/alliance-zone05
+uv run python process_zone.py --data-path /data/plant-rl/online/E12/P1/DiscreteRandom6/alliance-zone06
+uv run python process_zone.py --data-path /data/plant-rl/online/E12/P1/DiscreteRandom8/alliance-zone08
+uv run python process_zone.py --data-path /data/plant-rl/online/E12/P1/DiscreteRandom9/alliance-zone09
+uv run python process_zone.py --data-path /data/plant-rl/online/E12/P1/DiscreteRandom10/alliance-zone10
+uv run python process_zone.py --data-path /data/plant-rl/online/E12/P1/DiscreteRandom11/alliance-zone11
+uv run python process_zone.py --data-path /data/plant-rl/online/E12/P1/DiscreteRandom12/alliance-zone12
+
+uv run python process_zone.py --data-path /data/plant-rl/online/E13/P1/Dirichlet1/alliance-zone01
+uv run python process_zone.py --data-path /data/plant-rl/online/E13/P1/Dirichlet2/alliance-zone02
+uv run python process_zone.py --data-path /data/plant-rl/online/E13/P1/Dirichlet3/alliance-zone03
+uv run python process_zone.py --data-path /data/plant-rl/online/E13/P1/Dirichlet4/alliance-zone04
+uv run python process_zone.py --data-path /data/plant-rl/online/E13/P1/Dirichlet5/alliance-zone05
+uv run python process_zone.py --data-path /data/plant-rl/online/E13/P1/Dirichlet6/alliance-zone06
+uv run python process_zone.py --data-path /data/plant-rl/online/E13/P1/Dirichlet8/alliance-zone09
+uv run python process_zone.py --data-path /data/plant-rl/online/E13/P1/Dirichlet9/alliance-zone09
+uv run python process_zone.py --data-path /data/plant-rl/online/E13/P1/Dirichlet10/alliance-zone10
+uv run python process_zone.py --data-path /data/plant-rl/online/E13/P1/Dirichlet11/alliance-zone11
+uv run python process_zone.py --data-path /data/plant-rl/online/E13/P1/Dirichlet12/alliance-zone12
+
+uv run python process_zone.py --data-path /data/plant-rl/online/E14/P1/Constant1/alliance-zone01
+uv run python process_zone.py --data-path /data/plant-rl/online/E14/P1/InAC2/alliance-zone02
+uv run python process_zone.py --data-path /data/plant-rl/online/E14/P1/InAC3/alliance-zone03
+uv run python process_zone.py --data-path /data/plant-rl/online/E14/P1/InAC4/alliance-zone04
+uv run python process_zone.py --data-path /data/plant-rl/online/E14/P1/InAC5/alliance-zone05
+uv run python process_zone.py --data-path /data/plant-rl/online/E14/P1/InAC6/alliance-zone06
+uv run python process_zone.py --data-path /data/plant-rl/online/E14/P1/InAC8/alliance-zone08
+uv run python process_zone.py --data-path /data/plant-rl/online/E14/P1/InAC9/alliance-zone09
+uv run python process_zone.py --data-path /data/plant-rl/online/E14/P1/InAC10/alliance-zone10
+uv run python process_zone.py --data-path /data/plant-rl/online/E14/P1/InAC11/alliance-zone11
+uv run python process_zone.py --data-path /data/plant-rl/online/E14/P1/InAC12/alliance-zone12
+
+uv run python join_zones.py --root-dir /data/plant-rl/online
+uv run python create_minari_dataset.py --input-dir /data/plant-rl/offline
