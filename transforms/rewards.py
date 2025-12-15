22358e8e759444026a2f665adaca94fc29639d78
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-12-14 22:14:48 -0700

    feat: update reward function to be absolute difference in initial plant area units

--- transforms/rewards.py
+++ transforms/rewards.py
@@ -3,19 +3,27 @@ import polars as pl
 
 def transform_reward(df):
     df = df.with_columns(
+        pl.col("clean_area")
+        .over("plant_id")
+        .filter(pl.col("clean_area") > 0)
+        .first()
+        .alias("initial_clean_area")
+    )
+    df = df.with_columns(
         pl.col("clean_area").shift(1).over("plant_id").alias("prev_clean_area"),
     )
     df = df.with_columns(
         (
             (pl.col("clean_area") - pl.col("prev_clean_area"))
-            / pl.col("prev_clean_area")
+            / pl.col("initial_clean_area")
         ).alias("reward"),
     )
-    df = df.drop("prev_clean_area")
+    df = df.drop("initial_clean_area", "prev_clean_area")
     # if bolted, reward = 0
     df = df.with_columns(
-        pl.when(
-            pl.col("bolted_pred") > 0.5
-        ).then(0).otherwise(pl.col("reward")).alias("reward")
+        pl.when(pl.col("bolted_pred") > 0.5)
+        .then(0)
+        .otherwise(pl.col("reward"))
+        .alias("reward")
     )
     return df
